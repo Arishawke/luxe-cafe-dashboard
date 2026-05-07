57b7e18610737dea364eadbfc2623af412f182a0
Arishawke <github@fadewyrm.com>
2026-05-07 19:29:36 -0400

    Drop misleading sequencing comments in Clear All Data handler
    After useShots took over persistence, the "first/then" framing
    no longer applies (the hook's effect commits asynchronously).

--- src/App.tsx
+++ src/App.tsx
@@ -3006,11 +3006,9 @@ function App() {
                         'Clear All Data',
                         `Are you sure you want to delete ALL data? This will permanently remove ${shots.length} shots, ${recipes.length} recipes, and ${beans.length} beans. This action cannot be undone.`,
                         () => {
-                          // Clear localStorage first (shots persisted via useShots effect)
                           saveRecipes([]);
                           saveBeans([]);
                           saveFavorites({});
-                          // Then update state
                           setShots([]);
                           setRecipes([]);
                           setBeans([]);
