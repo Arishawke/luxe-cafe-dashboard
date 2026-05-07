43e6db15b6068ad25bf7335ca896b5896009ba7c
Arishawke <github@fadewyrm.com>
2026-05-07 19:16:42 -0400

    Extract bean helpers to lib/beans.ts; delete empty utils.ts

--- src/utils.ts
+++
@@ -1,6 +0,0 @@
-import type { ShotLog } from './types';
-
-export function getUniqueBeans(shots: ShotLog[]): string[] {
-    const unique = new Set(shots.map(s => s.beanName));
-    return Array.from(unique).sort();
-}
